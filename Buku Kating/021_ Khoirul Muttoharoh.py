a90f52742d5c890f8619eae5e5198ae48e2f9f9a
khoirulmuttoharoh <khoirulmuttoharoh@gmail.com>
2024-10-12 13:18:21 +0700

    upload foto mut

--- Buku Kating/021_ Khoirul Muttoharoh.py
+++ Buku Kating/021_ Khoirul Muttoharoh.py
@@ -97,8 +97,8 @@ menu = streamlit_menu()
 if menu == "Kesekjenan":
     def kesekjenan():
         gambar_urls = [
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
-            "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
+            "https://drive.google.com/uc?export=view&id=1UuDll6EvmsLLI6S18guq2qjdrljxNbND", #Bang Kharisma
+            "https://drive.google.com/uc?export=view&id=1lOd0lUoe9szDY62I-vUAa4UtXnO-qShl", #Bang Pandra
             "https://drive.google.com/uc?export=view&id=1tBo0l5pxH4N8o3rNk-Iupet4c12OATy_",
         ]
         data_list = [
