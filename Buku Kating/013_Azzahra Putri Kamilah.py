6e8fc94c9f73ad5e5851b625accb085a98e09d0c
fal.study35@gmail.com <fal.study35@gmail.com>
2024-11-04 22:46:27 +0700

    revisidata

--- Buku Kating/013_Azzahra Putri Kamilah.py
+++ Buku Kating/013_Azzahra Putri Kamilah.py
@@ -226,7 +226,7 @@ elif menu == "Baleg":
                 "pesan" : " mau dung kak di ajarin terus ngodingnya.. sabar2 dalam menghadapi zahra ya kak"
             },
             {
-                "nama"  : "Annisa Dini Amaliya",
+                "nama"  : "Anisa Dini Amaliya",
                 "nim"   : "121450081",
                 "umur"  : "21",
                 "asal"  : "Tanggerang",
@@ -281,7 +281,7 @@ elif menu == "Baleg":
                 "pesan" : "tetap optimis bang dan semangat selalu"
             },
             {
-                "nama"  : "Berlianda Enda Putri",
+                "nama"  : "Berliana Enda Putri",
                 "nim"   : "122450065",
                 "umur"  : "21",
                 "asal"  : "Sumatera Barat",
@@ -445,7 +445,7 @@ elif menu == "Departemen PSDA":
                 "pesan" : "semangat kuliahnyaa bang"
             },
             {
-                "nama"  : "Oktavia Nurwenda Puspita Sari",
+                "nama"  : "Oktavia Nurwinda Puspita Sari",
                 "nim"   : "122450041",
                 "umur"  : "20",
                 "asal"  : "Lampung Timur",
@@ -523,7 +523,7 @@ elif menu == "Departemen PSDA":
             },
             {
                 "nama"  : "Rafa Aqilla Jungjunan",
-                "nim"   : "122450148",
+                "nim"   : "122450142",
                 "umur"  : "20",
                 "asal"  : "Riau",
                 "alamat": "Belwis",
@@ -545,7 +545,7 @@ elif menu == "Departemen PSDA":
             },
             {
                 "nama"  : "Vanessa Olivia Rose",
-                "nim"   : "121450068",
+                "nim"   : "121450108",
                 "umur"  : "20",
                 "asal"  : "Jakarta",
                 "alamat": "Perumahan Korpri",
@@ -1274,7 +1274,7 @@ elif menu == "Departemen Internal":
             },
             {
                 "nama"  : "Catherine Firdhasari Maulina Sinaga",
-                "nim"   : "121450071",
+                "nim"   : "121450072",
                 "umur"  : "20",
                 "asal"  : "Sumatera Utara",
                 "alamat": "Airan",
@@ -1437,7 +1437,7 @@ elif menu == "Departemen SSD":
                 "pesan" : "semangat semester akhirnya bang"
             },
             {
-                "nama"  : "Adisty Syawaida Ariyanto",
+                "nama"  : "Adisty Syawalda Ariyanto",
                 "nim"   : "121450136",
                 "umur"  : "22",
                 "asal"  : "Metro",
